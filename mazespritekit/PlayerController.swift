47c6c6f761049357b6450ee7c47253a7e97704a0
AmFlint <amasselot@hotmail.com>
2019-05-15 18:48:19 +0200

    Add Realtime/online parties, sprites for Player2

--- mazespritekit/PlayerController.swift
+++ mazespritekit/PlayerController.swift
@@ -10,13 +10,13 @@ import Foundation
 import SpriteKit
 
 class PlayerController: PlayerControllerDelegate {
-    var playerSpriteNode = SKSpriteNode(imageNamed: "player")
+    var playerSpriteNode = SKSpriteNode(imageNamed: "player1")
     var playerRunFrames = [SKTexture]()
     var sprite: SKSpriteNode {
         return playerSpriteNode
     }
     
-    func setUpPlayerSprite(frame: CGRect, position:CGPoint, withCallback cb: (SKSpriteNode) -> Void) {
+    func setUpPlayerSprite(frame: CGRect, position: CGPoint, withCallback cb: (SKSpriteNode) -> Void) {
         playerSpriteNode.position = position
         playerSpriteNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 60, height: 60))
         playerSpriteNode.physicsBody?.isDynamic = true
@@ -24,13 +24,6 @@ class PlayerController: PlayerControllerDelegate {
         playerSpriteNode.physicsBody?.affectedByGravity = false
         playerSpriteNode.size = CGSize(width: playerSpriteNode.size.width * 0.15, height: playerSpriteNode.size.height * 0.15)
         
-        let textureAtlas = SKTextureAtlas(named: "Run Frames")
-        
-        
-        for i in 1..<textureAtlas.textureNames.count {
-            let textureName = "Run\(i)"
-            playerRunFrames.append(textureAtlas.textureNamed(textureName))
-        }
         cb(playerSpriteNode)
     }
     
@@ -51,10 +44,28 @@ class PlayerController: PlayerControllerDelegate {
             playerSpriteNode.xScale = playerSpriteNode.xScale * (-1)
         }
     }
+    
+    required init(frame: CGRect, playerNumber: Int, callback: (SKSpriteNode) -> Void) {
+        let spriteName = "player\(playerNumber)"
+        playerSpriteNode = SKSpriteNode(imageNamed: spriteName)
+        
+        guard let position = playerPosition[playerNumber] else { return }
+        
+        let textureAtlas = SKTextureAtlas(named: "Run Player \(playerNumber)")
+        
+        for i in 1..<textureAtlas.textureNames.count {
+            let textureName = "Run\(playerNumber)\(i)"
+            playerRunFrames.append(textureAtlas.textureNamed(textureName))
+        }
+        
+        self.setUpPlayerSprite(frame: frame, position: position, withCallback: callback)
+    }
 }
 
 protocol PlayerControllerDelegate {
     var sprite: SKSpriteNode { get }
     func run(direction: CGVector, velocity: CGVector)
-    func setUpPlayerSprite(frame: CGRect, position:CGPoint, withCallback cb: (SKSpriteNode) -> Void)
+    func setUpPlayerSprite(frame: CGRect, position: CGPoint, withCallback cb: (SKSpriteNode) -> Void)
+    
+    init(frame: CGRect, playerNumber: Int, callback: (SKSpriteNode) -> Void)
 }
