fa4ceaf77288b7707709f1eccbfb7177679f49e4
amasselot <amasselot@online.net>
2019-05-15 13:04:41 +0200

    Replace ball With sprite, enhance Player Controller / Delegate

--- mazespritekit/GameScene.swift
+++ mazespritekit/GameScene.swift
@@ -12,8 +12,9 @@ import CoreMotion
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
+    let playerDelegate: PlayerControllerDelegate = PlayerController()
+    
     let manager = CMMotionManager()
-    var ball:SKNode = SKNode()
     var goal:SKNode = SKNode()
     
     var velocity:CGVector? = CGVector()
@@ -22,13 +23,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.physicsWorld.contactDelegate = self
         
-        ball = self.childNode(withName: "ball")!
-        ball.physicsBody?.affectedByGravity = false
+        // set up player
+        playerDelegate.setUpPlayerSprite(frame: frame, withCallback: {
+            sprite in
+            addChild(sprite)
+        })
         
         goal = self.childNode(withName: "goal")!
         goal.physicsBody?.affectedByGravity = false
-        
-        
         
         manager.startAccelerometerUpdates()
         manager.accelerometerUpdateInterval = 0.05
@@ -37,14 +39,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let yaw = self.manager.deviceMotion?.attitude.yaw // droite gauche
             let roll = self.manager.deviceMotion?.attitude.roll // roulade
             
-            self.ball.physicsBody?.applyImpulse(CGVector(dx:CGFloat(Float(roll!)) , dy: CGFloat(Float(-1 * pit!))))
-
+            let runDirection = CGVector(dx:CGFloat(Float(roll!)) , dy: CGFloat(Float(-1 * pit!)))
+            self.playerDelegate.run(direction: runDirection)
             
         }
         manager.startAccelerometerUpdates(to: OperationQueue.main){ data,error in
             self.velocity = CGVector(dx: (data?.acceleration.x)! * 2.5, dy: (data?.acceleration.y)! * 2.5)
         }
-        
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
@@ -61,6 +62,5 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     override func update(_ currentTime: TimeInterval) {
         // nothing
     }
-    
 
 }
