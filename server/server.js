4eb429543acc94eefe49a530445c61c70b464e26
AmFlint <amasselot@hotmail.com>
2019-05-15 15:00:17 +0200

    Server

--- server/server.js
+++ server/server.js
@@ -4,11 +4,18 @@ var io = require('socket.io')(http);
 
 const players = {};
 
+const initialPlayer = {
+  x: 0,
+  y: 0
+};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  console.log('user connected to socket');
+
   socket.on('player_move', function(player) {
     if (!player.id || !players[player.id]) return;
     const currentPlayer = players[player.id];
@@ -20,10 +27,10 @@ io.on('connection', function(socket){
   // When a player logs in to the room
   socket.on('player_login', function(player) {
     if (!player.id || players[player.id]) return;
-    players[player.id] = Object.assign({}, player, { socketId: socket.conn.id, number: Object.values(players).length + 1 });
+    players[player.id] = Object.assign(initialPlayer, player, { socketId: socket.conn.id, number: Object.values(players).length + 1 });
     console.log('A Player has logged in')
     console.log(players);
-    socket.emit('player_logged_in', player);
+    socket.emit('player_logged_in', players[player.id]);
   });
 
   socket.on('disconnect', function() {
